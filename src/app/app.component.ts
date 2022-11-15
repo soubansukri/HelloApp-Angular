f3c4bc88f3952c46dc035d0b822288c40fa9cdaf
soubansukri <souban.sukri@gmail.com>
2022-11-15 13:03:00 +0530

    Added: BL image using property data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Demo';
-
+  imgUrl="";
   ngOnInit():void{
     this.title="Hello from Bridgelabz"
+    this.imgUrl="assets/images/BridgeLabzImage.jpg";
   }
 }
